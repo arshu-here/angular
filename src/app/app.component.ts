2201a01deeb5070b80e96a9c0d59f780e9109b00
Akshat <akshat@Akshat.Akshat>
2023-03-25 18:40:23 +0530

    basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'helloworld';
+  title:any;
+  colorOfText="unknown";
+  salary!:number;
+  color='elseColor';
+
+  fruits:string[] = ['mango', 'apple', 'banana', 'leechi'];
+
+  constructor() {
+    this.title = 'hello world';
+    this.salary = 100;
+  }
+
+  checkSalary() {
+    return this.salary > 100;
+  }
+
+  changeTitle() {
+   if(this.salary <= 100)
+   {
+    this.salary = 200;
+   }else {
+    this.salary = 100;
+   }
+  }
+
 }
